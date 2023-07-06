175ecffb65dd221c29a1decff667dbebd99ed37b
shahjavadeveloper <shahjavadeveloper@gmail.com>
2023-07-06 15:47:46 +0530

    first commit

---
+++ src/main/java/com/shah/jenkinsapi/SpringJenkins1Application.java
@@ -0,0 +1,20 @@
+package com.shah.jenkinsapi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkins1Application {
+
+	
+	private static final Logger log = LoggerFactory.getLogger(SpringJenkins1Application.class);
+
+	public static void main(String[] args) {
+		log.info("Application executed......");
+		
+		SpringApplication.run(SpringJenkins1Application.class, args);
+	}
+
+}
